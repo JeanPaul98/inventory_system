c16a838787f0b9d4dc5aed35d851fbe0bdae19b8
jean paul <89642411+JeanPaul98@users.noreply.github.com>
2024-07-09 09:34:19 +0200

    first add

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Inventory System © {{ date('Y') }} || Developed by <strong><a target="_blank" href="https://verim-solution.com/">Verim-solution</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version<strong class="text-danger">1.0</strong></div>
+</footer>
